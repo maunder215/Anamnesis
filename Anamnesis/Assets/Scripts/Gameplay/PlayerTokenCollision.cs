c43625b23d820862b54ab33bfe3047d7be3801e8
maunder215 <maunder215@gmail.com>
2024-12-06 22:23:49 -0600

    Added credits and score counter.

--- Anamnesis/Assets/Scripts/Gameplay/PlayerTokenCollision.cs
+++ Anamnesis/Assets/Scripts/Gameplay/PlayerTokenCollision.cs
@@ -2,6 +2,7 @@ using Platformer.Core;
 using Platformer.Mechanics;
 using Platformer.Model;
 using UnityEngine;
+using TMPro;
 
 namespace Platformer.Gameplay
 {
@@ -13,12 +14,18 @@ namespace Platformer.Gameplay
     {
         public PlayerController player;
         public TokenInstance token;
+        int currScore = 0;
 
         PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+        // Tex score = GameObject respawn = GameObject.FindGameObjectsWithTag("Spawn Point")[0];
+        TextMeshProUGUI score = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+
 
         public override void Execute()
         {
             AudioSource.PlayClipAtPoint(token.tokenCollectAudio, token.transform.position);
+            currScore++;
+            score.text = "Score: " + currScore;
         }
     }
 }
